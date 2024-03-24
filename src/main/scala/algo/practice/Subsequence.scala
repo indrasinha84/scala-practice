d6b5865e73c1abaa35d33f0d3a65ec331e279f08
indrasinha84 <38633558+indrasinha84@users.noreply.github.com>
2024-03-24 18:37:36 +0530

    Used maxBy.

--- src/main/scala/algo/practice/Subsequence.scala
+++ src/main/scala/algo/practice/Subsequence.scala
@@ -5,10 +5,10 @@ object Subsequence:
   def main(args: Array[String]): Unit =
     val line = "MKHKGJJGGH;XMKGJGRJH"
     val Array(left, right) = line.split(';')
-    println(findSequence(left, right, "").head)
+    println(findSequence(left, right, "").maxBy(_.length))
 
 
   private def findSequence(left: String, right: String, result: String): Seq[String] =
     left.flatMap(c => right.split(c.toString, 2) match
       case Array(_, remaining) => findSequence(left.tail, remaining, result + c)
-      case _ => Seq(result)).sortBy(-_.length)
+      case _ => Seq(result))
