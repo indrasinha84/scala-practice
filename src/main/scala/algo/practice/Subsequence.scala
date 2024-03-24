b758506423a23bafb5d5e921145321cd12a2e024
indrasinha84 <38633558+indrasinha84@users.noreply.github.com>
2024-03-24 19:50:04 +0530

    Code fix

--- src/main/scala/algo/practice/Subsequence.scala
+++ src/main/scala/algo/practice/Subsequence.scala
@@ -3,11 +3,15 @@ package algo.practice
 object Subsequence:
 
   def main(args: Array[String]): Unit =
-    val line = "MKHKGJJGGH;XMKGJGRJH"
+    val line = "MKHKGJJGGH;XMKGJJGRJGH"
     val Array(left, right) = line.split(';')
-    println(findSequence(left, right, "").maxBy(_.length))
+    val xxx = findSequence(left, right, "")
+    println(xxx.maxBy(_.length))
 
   private def findSequence(left: String, right: String, result: String): Seq[String] =
-    left.flatMap(c => right.split(c.toString, 2) match
-      case Array(_, remaining) => findSequence(left.split(c.toString, 2).last, remaining, result + c)
-      case _ => Seq(result))
+    if left.isEmpty then
+      Seq(result)
+    else
+      left.flatMap(c => right.split(c.toString, 2) match
+        case Array(_, remaining) => findSequence(left.split(c.toString, 2).last, remaining, result + c)
+        case _ => Seq(result))
