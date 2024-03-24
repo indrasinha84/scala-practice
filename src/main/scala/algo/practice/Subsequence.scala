fd4e5b57db9d3355032dbbb3fccedf4d818fda4c
indrasinha84 <38633558+indrasinha84@users.noreply.github.com>
2024-03-24 20:19:59 +0530

    Tail recursive implementation.

--- src/main/scala/algo/practice/Subsequence.scala
+++ src/main/scala/algo/practice/Subsequence.scala
@@ -1,11 +1,14 @@
 package algo.practice
 
+import scala.annotation.tailrec
+
 object Subsequence:
 
   def main(args: Array[String]): Unit =
     val line = "MKHKGJJGGH;XMKGJJGRJGH"
     val Array(left, right) = line.split(';')
     println(findSequence(left, right, "").maxBy(_.length))
+    println(findSequenceWithTailRec(left, right, ""))
 
   private def findSequence(left: String, right: String, result: String): Seq[String] =
     if left.isEmpty then
@@ -15,3 +18,14 @@ object Subsequence:
         case Array(_, remaining) => findSequence(left.split(c.toString, 2).last, remaining, result + c)
         case _ => Seq(result)
       )
+
+  @tailrec
+  private def findSequenceWithTailRec(left: String, right: String, result: String): String =
+    if left.isEmpty || right.isEmpty then
+      result
+    else
+      val positionInRight = right.lastIndexOf(left.last)
+      if positionInRight == -1 then
+        findSequenceWithTailRec(left.init, right, result)
+      else
+        findSequenceWithTailRec(left.init, right.take(positionInRight), left.last + result)
