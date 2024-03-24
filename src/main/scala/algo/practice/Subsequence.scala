37d5e5464d99d2af05de9374f21b73941d639fa6
indrasinha84 <38633558+indrasinha84@users.noreply.github.com>
2024-03-24 18:41:15 +0530

    Extra space.

--- src/main/scala/algo/practice/Subsequence.scala
+++ src/main/scala/algo/practice/Subsequence.scala
@@ -7,7 +7,6 @@ object Subsequence:
     val Array(left, right) = line.split(';')
     println(findSequence(left, right, "").maxBy(_.length))
 
-
   private def findSequence(left: String, right: String, result: String): Seq[String] =
     left.flatMap(c => right.split(c.toString, 2) match
       case Array(_, remaining) => findSequence(left.tail, remaining, result + c)
